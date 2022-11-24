659260504b77c9a8b44446359d640e7c69301bd5
un-know-n <fa1thlezss@gmail.com>
2022-11-24 21:14:33 +0200

    GOODS-[FEAT]: Search optimization
    Created useDebounce hook, updated UX

--- goods-shop/src/hooks/useDebounce.ts
+++ goods-shop/src/hooks/useDebounce.ts
@@ -1,3 +1,25 @@
-export const useDebounce = () => {
-  return 0;
+import { useCallback, useRef } from 'react';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const useDebounce = (callback: any, delay: number) => {
+  //Variable to store the timer id
+  const timer = useRef<any>(null);
+
+  //Take the link of the function, depending on callback and delay
+  const debouncedCallback = useCallback(
+    (...args: any) => {
+      //If the timer has already been initialized -> clear it
+      if (timer.current) clearTimeout(timer.current);
+
+      //Create new timer
+      timer.current = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    },
+    [callback, delay],
+  );
+
+  return debouncedCallback;
 };
+
+//TODO: Fix the any type to something more appropriate!
